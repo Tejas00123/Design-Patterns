aeb44098e70e7c2c602096df25127aa30ab33107
Tejas Gavali <tejasgavali8280@gmail.com>
2024-10-07 13:40:21 +0530

    pkg , Test file created by dev1

--- SingleTonDP/src/com/nt/test/Test.java
+++ SingleTonDP/src/com/nt/test/Test.java
@@ -1,7 +1,7 @@
 package com.nt.test;
 
 public class Test {
-   public static void main(String[] args) {
-	   
+   public static void main(String[] args){
+	   System.out.println("=====Singleton design pattern======");
    }
 }
